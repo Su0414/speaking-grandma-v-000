72001c05cfb32e09f6455d1013ae62c4f847799e
Suchitra Naik <susanaik@gmail.com>
2018-04-23 09:42:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(string)
   
   if(string == string.upcase)
     return "NO, NOT SINCE 1938!"
-  elsif(string == "I LOVE YOU GRANDMA!" || string =="i love you grandma!")
+  elsif(string == "I LOVE YOU GRANDMA!")
    return "I LOVE YOU TOO PUMPKIN!"
  else
    return "HUH?! SPEAK UP, SONNY!"
